86cdbffec937cc9f82e1704f08cff9d0b5669be3
hskeays <hskeays@gmail.com>
2024-06-01 20:23:58 -0400

    netlify form submission test

--- js/main.js
+++ js/main.js
@@ -81,34 +81,3 @@ function validateForm(event) {
 }
 
 document.getElementById("contactForm").addEventListener("submit", validateForm);
-
-exports.handler = async (event, context) => {
-  if (event.httpMethod !== 'POST') {
-    return {
-      statusCode: 405,
-      body: JSON.stringify({ error: 'Method Not Allowed' })
-    };
-  }
-
-  const formData = JSON.parse(event.body);
-
-  // Process form data (send email, save to database, etc.)
-  // Example: sending an email
-  const { firstName, lastName, email, subject, message } = formData;
-  const emailBody = `
-    First Name: ${firstName}\n
-    Last Name: ${lastName}\n
-    Email: ${email}\n
-    Subject: ${subject}\n
-    Message: ${message}\n
-  `;
-
-  // Send email using a service like SendGrid, Nodemailer, etc.
-  // Example:
-  // sendEmail(emailBody);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Form submitted successfully' })
-  };
-};
